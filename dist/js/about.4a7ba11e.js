3d4479666a44dd2e14d775837d1bfd475f56b7f3
aurassai <b-raychev@yandex.com>
2019-02-17 21:53:02 +0200

    build

--- dist/js/about.4a7ba11e.js
+++ dist/js/about.4a7ba11e.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About")]),n("p",[t._v("\n    This is the TodoList app v1.0.0. It is made as a Vue training\n    project, using JSON placeholder as a fake REST API.\n  ")])])}],i=n("2877"),u={},o=Object(i["a"])(u,a,s,!1,null,null,null);e["default"]=o.exports}}]);
-//# sourceMappingURL=about.cd0f1704.js.map
+//# sourceMappingURL=about.4a7ba11e.js.map
